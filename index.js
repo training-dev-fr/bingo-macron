cb343e32f9b6cb0a0591ab8e6810834900023f11
training-dev <contact@training-dev.fr>
2024-12-31 14:37:51 +0100

    fix mobile

--- index.js
+++ index.js
@@ -153,6 +153,13 @@ document.querySelectorAll(".case").forEach(mot => {
         if (!e.currentTarget.classList.contains("active")) {
             e.currentTarget.classList.add("active");
         }
-    })
-    
+    });
+});
+
+document.querySelector(".info").addEventListener("click",()=> {
+    document.querySelector(".modal").classList.add("show");
+})
+
+document.querySelector(".modal-close").addEventListener("click",() => {
+    document.querySelector(".modal").classList.remove("show");
 })
